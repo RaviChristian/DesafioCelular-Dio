14a9f1400e580baa1b0c833df571eae836e51a33
Ravi Christian <108839457+RaviChristian@users.noreply.github.com>
2024-01-06 12:20:12 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using DesafioPOO.Models;
+
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Teste com Nokia
+            Console.WriteLine("Teste com Nokia:");
+            Nokia nokia = new Nokia("123456789", "Nokia123", "IMEI123", 64);
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
+            Console.WriteLine();
+
+            // Teste com iPhone
+            Console.WriteLine("Teste com iPhone:");
+            Iphone iphone = new Iphone("987654321", "iPhoneX", "IMEI987", 128);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+            Console.WriteLine();
+
+            Console.ReadLine();  // Manter o console aberto para visualizar os resultados
+        }
+    }
+}
